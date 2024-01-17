0e1416f2c0ce3e9dd7233a6ccec39c6f5b8a2fdc
Yrwom <139040533+Yrwom@users.noreply.github.com>
2024-01-17 00:07:43 +0000

    Added Unit Test Outcome

--- .ccls-cache/@home@runner@PersonalInfo/main.c
+++ .ccls-cache/@home@runner@PersonalInfo/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Lucinda Pottern\n6/24/1992\nwork 000-000-0101\ncell 000-000-0189\n \n");
-  printf("John Smith\n 8/15/1978\nwork 000-111-0101\ncell 000-111-0189\n \n");
-  printf("Chris Johnson\n12/01/1987\nwork 000-222-0101\ncell 000-222-0189\n \n");
-  printf("Robert Hall\n2/27/1949\nwork 000-444-0101\ncell 000-444-0189\n \n");
+  printf("Unit Test #1 Expected Outcome\nLucinda Pottern\n6/24/1992\nwork 000-000-0101\ncell 000-000-0189\n \n");
+  printf("Unit Test #2 Expected Outcome\nJohn Smith\n 8/15/1978\nwork 000-111-0101\ncell 000-111-0189\n \n");
+  printf("Unit Test #3 Expected Outcome\nChris Johnson\n12/01/1987\nwork 000-222-0101\ncell 000-222-0189\n \n");
+  printf("Unit Test #4 Expected Outcome\nRobert Hall\n2/27/1949\nwork 000-444-0101\ncell 000-444-0189\n \n");
   return 0;
 }
